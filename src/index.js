7dbc5c3f87093277fbb2b18b3f2cf2091a72ec06
radville <laura.radville@gmail.com>
2019-10-24 10:22:13 -0400

    Done.

--- src/index.js
+++ src/index.js
@@ -2,8 +2,6 @@ const addBtn = document.querySelector('#new-toy-btn')
 const toyForm = document.querySelector('.container')
 let addToy = false
 
-// YOUR CODE HERE
-
 addBtn.addEventListener('click', () => {
   // hide & seek with the form
   addToy = !addToy
@@ -14,5 +12,51 @@ addBtn.addEventListener('click', () => {
   }
 })
 
+document.addEventListener("DOMContentLoaded", function() {
+  return fetch('http://localhost:4000/toys')
+  .then(function(response) {
+    return response.json();
+  }).then(function(json) {
+    for (const element of json) {
+    let card = document.createElement("div");
+    card.classList.add("card");
+    document.getElementById("toy-collection").appendChild(card)
+    let h2 = document.createElement("h2");
+    h2.innerHTML = element.name;
+    card.appendChild(h2);
+    let img = document.createElement("img");
+    img.src = element.image;
+    img.classList.add("toy-avatar");
+    card.appendChild(img);
+    card.appendChild(document.createElement("p"));
+    button = document.createElement("button");
+    button.classList.add("like-btn");
+    var text = document.createTextNode("Like <3"); 
+    button.appendChild(text);
+    card.appendChild(button);
+    }  
+});
+})
 
-// OR HERE!
+let formData = {
+  name: document.querySelector(".input-text").value,
+  image: document.querySelector(".input-text + br + .input-text").value,
+  likes: 0
+};
+ 
+let configObj = {
+  method: "POST",
+  headers: {
+    "Content-Type": "application/json",
+    "Accept": "application/json"
+  },
+  body: JSON.stringify(formData)
+};
+ 
+fetch("http://localhost:4000/toys", configObj)
+  .then(function(response) {
+    return response.json();
+  })
+  .then(function(object) {
+    console.log(object);
+  });
